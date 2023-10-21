8d7f8161f1a0ed39a47865502ec68db2b9b4873b
pradiptechie <kumarpradip3956@gmail.com>
2023-10-21 15:22:27 +0545

    sat

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 This is a MINI project that take text or image  as input and apply it as watermark on your Photo and gives download option for output image.
 
 https://github.com/pradiptechie/Image-Watermark-Tool.git
+
+
+.then((response) => response.text()) // Convert the response to text
+        .then((dataUrl) => {
+        // Create an image element and set its src attribute to the data URL
+        const imageElement = document.createElement('img');
+        imageElement.src = dataUrl;
+
+        // Clear the result container and append the image element
+        preview.innerHTML = '';
+        preview.appendChild(imageElement);
+
+        // Set the download link's href and display it
+        download.href = dataUrl;
+        download.style.display = 'block';
+        })
+        .catch((error) => {
+        console.error('Error:', error);
+        });
--- index.js
+++ index.js
@@ -20,45 +20,47 @@ app.get('/', (req, res) => {
 })
 
 app.post("/", upload.any(), (req, res) => {
-    const mainImageFile = req.files.find((file) => file.fieldname === 'mainImage');
-    const watermarkFile = req.files.find((file) => file.fieldname === 'watermarkImage');
-    const textWatermark = req.body.textWatermark;
+    const mainImage = req.files.find((file) => file.fieldname === 'mainImage');
     const watermarkType = req.body.watermarkType;
+    const watermarkImage = req.files.find((file) => file.fieldname === 'watermarkImage');
+    const watermarkText = req.body.watermarkText;
 
-    console.log(mainImageFile);
+    console.log(mainImage);
     console.log(watermarkType);
-    console.log(watermarkFile);
-    console.log(textWatermark);
+    console.log(watermarkImage);
+    console.log(watermarkText);
 
-    // if (!mainImageFile) {
-    //     return res.redirect('/');
-    // }
+    if (!mainImage) {
+        return res.redirect('/');
+    }
     
-    // const mainImageBuffer = mainImageFile.buffer;
+    const mainImageBuffer = mainImage.buffer;
 
-    // if (watermarkType === 'image' && watermarkFile) {
-    //     const watermarkBuffer = watermarkFile.buffer;
+    if (watermarkType === 'image' && watermarkImage) {
+        const watermarkBuffer = watermarkImage.buffer;
 
-    //     sharp(mainImageBuffer)
-    //         .composite([{ input: watermarkBuffer }])
-    //         .toBuffer()
-    //         .then((processedImage) => {
-    //             // Set the content type to display the image
-    //             res.set('Content-Type', 'image/jpeg');
-    //             res.send(processedImage);
-    //         });
-    // } else if (watermarkType === 'text' && textWatermark) {
-    //     sharp(mainImageBuffer)
-    //         .composite([{ input: Buffer.from(textWatermark) }])
-    //         .toBuffer()
-    //         .then((processedImage) => {
-    //             // Set the content type to display the image
-    //             res.set('Content-Type', 'image/jpeg');
-    //             res.send(processedImage);
-    //         });
-    // } else {
-    //     return res.redirect('/');
+        sharp(mainImageBuffer)
+            .composite([
+              {
+                input: watermarkBuffer,
+                gravity: 'southeast', // Position the watermark (southeast = bottom-right)
+              }
+            
+            ])
+            .toBuffer()
+            .then((processedImage) => {
+                // Set the content type to display the image
+                res.set('Content-Type', 'image/jpeg');
+                res.end(processedImage);
+            }).catch((error) => {
+                console.error('Error:', error);
+                res.status(500).send('Image processing error');
+            });
+    }
+    // else if (watermarkType === 'text' && watermarkText) {
+
     // }
+  
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
